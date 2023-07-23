b9cf2e7b8fe3c9266a78f3e8a9c1cc396bc802e4
Melvice Guimfack <melvice-junior.guimfack@polymtl.ca>
2023-07-23 19:38:57 -0400

    adding images to project

--- src/assets/projects/projects.js
+++ src/assets/projects/projects.js
@@ -1,27 +1,37 @@
+import robot from '../362238111_1361112497852996_7204551586153655371_n.jpg'
+import musicWeb from '../musicWeb.jpg'
+import VQ from '../VQ.jpg'
+import robotMaster from '../robotMaster.jpg'
+
+
 const projectList = [
   {
-    title: 'Project 1',
+    id: 1,
+    title: 'Embedded system',
     desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptate.',
-    image: 'https://picsum.photos/200/300',
+    image: robot,
     link: ''
   },
   {
-    title: 'Project 2',
+    id: 2,
+    title: 'Web difference game',
     desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptate.',
-    image: 'https://picsum.photos/200/300',
-    link: ''
+    image: VQ,
+    link: 'https://polytechnique-montr-al.gitlab.io/log2990/20231/equipe-208/LOG2990-208'
   },
   {
-    title: 'Project 3',
+    id: 3,
+    title: 'Music website',
     desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptate.',
-    image: 'https://picsum.photos/200/300',
-    link: ''
+    image: musicWeb,
+    link: 'https://melvice.github.io/MusicWebsite/music'
   },
   {
-    title: 'Project 4',
+    id: 4,
+    title: 'Robotic competition',
     desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptate.',
-    image: 'https://picsum.photos/200/300',
-    link: ''
+    image: robotMaster,
+    link: 'https://www.polystarmtl.com/'
   },
   ]
 
